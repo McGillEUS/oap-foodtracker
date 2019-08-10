33e47fbcd594e09cb5bfa025ad245d14497de91e
ahale <a.hale@live.ca>
2019-08-10 00:10:31 -0400

    add chmod & chown now that cron job is being run by root

--- app/gitManagement.sh
+++ app/gitManagement.sh
@@ -4,3 +4,6 @@ git commit -m "food history update";
 git pull;
 git push;
 chmod 777 /srv/www/oap/foodtracker/data/*;
+chown oap:oap /srv/www/oap/foodtracker/data/*
+chmod 775 /srv/www/oap/foodtracker/app/*;
+chown oap:oap /srv/www/oap/foodtracker/app/*
