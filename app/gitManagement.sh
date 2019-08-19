da20f59d3bf52b060bc4f9228d4f3e1b1f951552
alexanderhale <a.hale@live.ca>
2019-08-18 20:34:41 -0400

    remove pull & push (in hopes that cron job will work)

--- app/gitManagement.sh
+++ app/gitManagement.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 git add /srv/www/oap/foodtracker/data/*;
 git commit -m "food history update";
-git pull;
-git push;
 chmod 777 /srv/www/oap/foodtracker/data/*;
 chown oap:oap /srv/www/oap/foodtracker/data/*
 chmod 775 /srv/www/oap/foodtracker/app/*;
